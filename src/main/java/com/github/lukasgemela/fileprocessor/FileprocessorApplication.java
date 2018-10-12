a7ed99fe9286e33f8cac957fab06c140be170f2b
Lukas Gemela <lukas.gemela@sky.uk>
2018-10-12 14:41:51 +0100

    first commit

---
+++ src/main/java/com/github/lukasgemela/fileprocessor/FileprocessorApplication.java
@@ -0,0 +1,12 @@
+package com.github.lukasgemela.fileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileprocessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileprocessorApplication.class, args);
+    }
+}
